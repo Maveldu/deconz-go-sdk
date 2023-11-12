bc6386153e51476d0c1cc655e376d228775c1794
Mavel D <maveldussartre@gmail.com>
2023-11-12 15:32:37 +0000

    feat: added sdk base and Get

---
+++ sdk_test.go
@@ -0,0 +1,37 @@
+package sdk_test
+
+import (
+	"testing"
+
+	"github.com/maxatome/go-testdeep/td"
+	"github.com/jarcoal/httpmock"
+
+	"github.com/Maveldu/deconz-go-sdk"
+)
+
+func TestGet(t *testing.T) {
+	httpmock.Activate()
+	t.Cleanup(httpmock.DeactivateAndReset)
+
+	client := sdk.NewClient("https://test.bidule")
+
+	httpmock.RegisterResponder("GET", "https://test.bidule/lel", httpmock.NewStringResponder(200, `{"lul": 12}`))
+	httpmock.RegisterResponder("GET", "https://test.bidule/lel?lal=13", httpmock.NewStringResponder(200, `{"lul": 14}`))
+	httpmock.RegisterResponder("GET", "https://test.bidule/lil", httpmock.NewStringResponder(500, "nope"))
+
+	type lul struct {
+		Lul int64 `json:"lul"`
+	}
+	type lel struct {
+		Lal int `url:"lal"`
+	}
+	result := &lul{}
+
+	td.CmpNil(t, client.Get("/lel", nil, result))
+	td.Cmp(t, result, &lul{Lul: 12})
+
+	td.CmpNil(t, client.Get("/lel", &lel{Lal: 13}, result))
+	td.Cmp(t, result, &lul{Lul: 14})
+
+	td.Cmp(t, client.Get("/lil", nil, result), &sdk.Error{Status: "500", StatusCode: 500})
+}
